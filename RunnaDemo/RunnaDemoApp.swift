d5fa49854de55906f2b99d5d431b11c3789bd5ff
Amish Gupta <amishgupta678@gmail.com>
2024-12-03 18:08:32 -0800

    Initial Commit

---
+++ RunnaDemo/RunnaDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  RunnaDemoApp.swift
+//  RunnaDemo
+//
+//  Created by Amish Gupta on 12/3/24.
+//
+
+import SwiftUI
+
+@main
+struct RunnaDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
